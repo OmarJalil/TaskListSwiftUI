544ebe95b161506402d67853c83e8a6743507e41
jalil <omarjalilfierro@gmail.com>
2020-07-31 15:18:32 -0600

    Initial Commit

---
+++ Shared/TaskListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskListApp.swift
+//  Shared
+//
+//  Created by Omar on 31/07/20.
+//
+
+import SwiftUI
+
+@main
+struct TaskListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
